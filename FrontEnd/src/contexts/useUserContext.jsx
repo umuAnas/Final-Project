8165c1a7a12a4da5509153ad76c56e02920c4d8a
umuAnas <umuhuzyfa27@gmail.com>
2026-09-12 08:38:43 -0700

    some change in useUserContext

--- FrontEnd/src/contexts/useUserContext.jsx
+++ FrontEnd/src/contexts/useUserContext.jsx
@@ -1,10 +1,11 @@
-import {useContext} from "react";
-import {UserContext} from "../contexts/usercontext.jsx"
 
-export const useUserContext = () => {
-  const ctx = useContext(UserContext);
-  if (!ctx) {
-    throw new Error("useUserContext must be used within a UserContextProvider");
-  }
-  return ctx;
-};
+import UserContext from "./usercontext"
+import { useContext } from "react"
+
+export const useUserContext = ()=>{
+    const ctx=useContext(UserContext)
+    if(!ctx){
+    throw new Error("context must be provided")
+    }
+    return ctx
+}
